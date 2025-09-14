0eef942ef0f3a1972d203057515f0d6adb52a545
https-bittcode.github.io- <nikhilprajapati2370@gmail.com>
2025-09-14 12:59:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyASSaCcT6gK2Fn2awLJoyTYjYI-eYGOWjg",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:728517154808:web:4ca2258140bd936f11280d",
   measurementId: "G-BZJ3L4EDMR"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
